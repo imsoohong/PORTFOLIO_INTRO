164cc0778f081ed2367c5f0545b7b5514442c609
imsoohong <75714615+imsoohong@users.noreply.github.com>
2021-02-13 17:14:45 +0900

    JS: Navabar

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// Make navbar transparent when it is on th top
+const navbar = document.querySelector('#navbar');
+
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  // console.log(window.scrollY);
+  // console.log('navbarHeight: ${navbarHeight}');
+  if(window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else{
+    navbar.classList.remove('navbar--dark');
+  }
+})
