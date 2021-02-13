68f46ec373c431fe9ee9b140e7a20abef8d5c684
imsoohong <75714615+imsoohong@users.noreply.github.com>
2021-02-13 20:11:44 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 // Make navbar transparent when it is on th top
 const navbar = document.querySelector('#navbar');
-
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
@@ -17,6 +16,7 @@ document.addEventListener('scroll', () => {
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
+
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
@@ -24,8 +24,18 @@ navbarMenu.addEventListener('click', (event) => {
     return;
   } 
 
-  console.log(event.target.dataset.link); // 링크가 있어야 클릭이 됨//
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: 'smooth'});
+  // console.log(event.target.dataset.link); // 링크가 있어야 클릭이 됨//
+  scrollIntoView(link);
 
 });
+
+// Handle click on "Contact me " button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+}
