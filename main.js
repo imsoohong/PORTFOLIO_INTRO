52e9da46819a66fe7cab48b79cc24c0390677927
imsoohong <75714615+imsoohong@users.noreply.github.com>
2021-02-13 17:42:33 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -8,9 +8,24 @@ const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   // console.log(window.scrollY);
   // console.log('navbarHeight: ${navbarHeight}');
-  if(window.scrollY > navbarHeight) {
+  if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
-  } else{
+  } else {
     navbar.classList.remove('navbar--dark');
   }
 })
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if(link == null) { 
+    return;
+  } 
+
+  console.log(event.target.dataset.link); // 링크가 있어야 클릭이 됨//
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+
+});
