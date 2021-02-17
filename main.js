6c7cfa78a0d8d14439e9294bc97c05e395075fde
imsoohong <75714615+imsoohong@users.noreply.github.com>
2021-02-17 19:50:11 +0900

    insert data

--- main.js
+++ main.js
@@ -57,7 +57,28 @@ arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 });
 
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  console.log(filter);
+  projects.forEach((project) => {
+    console.log(project.dataset.type);
+    if (filter === '*' || filter === project.dataset.type) {
+      project.classList.remove('invisible');
+    } else {
+      project.classList.add('invisible');
+    }
+  });
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
-}
+};
