24493ae36f1c85ecc3ce1be3dcebc35e4d3145f7
imsoohong <75714615+imsoohong@users.noreply.github.com>
2021-02-19 11:48:07 +0900

    JS: Navbar menu toglle

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ document.addEventListener('scroll', () => {
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
-
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
@@ -24,8 +23,16 @@ navbarMenu.addEventListener('click', (event) => {
     return;
   }
 
+  // navbarMenu.classList.remove('open');
+
   // console.log(event.target.dataset.link); // 링크가 있어야 클릭이 됨//
   scrollIntoView(link);
+}); 
+
+// Navbar toggle button for small screen 
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
 // Handle click on "Contact me " button on home
