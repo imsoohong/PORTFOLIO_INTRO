2568057a01850e055383d1eeec792350380553ba
imsoohong <75714615+imsoohong@users.noreply.github.com>
2021-02-21 14:17:55 +0900

    End

--- main.js
+++ main.js
@@ -78,8 +78,8 @@ workBtnContainer.addEventListener('click', (e) => {
   // Remove selection from the previous item and select the new one
   const active = document.querySelector('.category__btn.selected');
   active.classList.remove('selected');
-  const target = e.target.nodeName === 'BUTTON' ? e.target : 
-                 e.target.parentNode;
+  const target = e.target.nodeName === 'BUTTON' ? e.target :
+    e.target.parentNode;
   target.classList.add('selected');
 
   projectContainer.classList.add('anim-out');
@@ -96,7 +96,71 @@ workBtnContainer.addEventListener('click', (e) => {
   }, 300);
 });
 
+// 1. 모든 섹션 요소들과 메뉴 아이템들을 가지고 온다.
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#testimonials',
+  '#contact'
+];
+
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+// console.log(sections);
+// console.log(navItems);
+
+let selectednNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove('active');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+}
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
+  selectNavItem(navItems[sectionsIds.indexOf(selector)]);
 };
+
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.3
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectednNavIndex = index + 1;
+      } else {
+        selectednNavIndex = index - 1;
+      }
+      // console.log(index, entry.target.id);
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('whell', () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    Math.round(window.scrollY + window.innerHeight) >=
+    document.body.clientHeight) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selecteNavIndex]);
+});
