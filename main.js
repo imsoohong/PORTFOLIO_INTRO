939f843d0813ea7bde9a6f9dded100d13be6b172
imsoohong <75714615+imsoohong@users.noreply.github.com>
2021-02-22 16:47:07 +0900

    JS: Modify

--- main.js
+++ main.js
@@ -76,11 +76,11 @@ workBtnContainer.addEventListener('click', (e) => {
   }
 
   // Remove selection from the previous item and select the new one
-  const active = document.querySelector('.category__btn.selected');
-  active.classList.remove('selected');
   const target = e.target.nodeName === 'BUTTON' ? e.target :
     e.target.parentNode;
   target.classList.add('selected');
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
 
   projectContainer.classList.add('anim-out');
   setTimeout(() => {
